f27bc69dd186ca12449354c58313eb123cfc8dc9
Hayyim <b2banalytica@gmail.com>
2023-08-21 16:58:42 +0100

    Wrap Up Getting Started

---
+++ Messenger/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ConversationsViewController.swift
+//  Messenger
+//
+//  Created by vitasiy on 21/08/2023.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
